2aab23dbdf997287cd7f9025246ed274318c8711
Israel Manzo <israelmanzo814@gmail.com>
2018-05-03 12:58:16 -0400

    Adding notificatin center project

---
+++ NotificationCenter+Observer/NotificationCenter+Observer/PickSideTeamVC.swift
@@ -0,0 +1,72 @@
+//
+//  PickSideTeamVC.swift
+//  NotificationCenter+Observer
+//
+//  Created by Israel Manzo on 5/3/18.
+//  Copyright © 2018 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class PickSideTeamVC: UIViewController {
+    
+    let captainColor = UIColor(red: 0/255, green: 26/255, blue: 77/255, alpha: 1)
+    let ironManColor = UIColor(red: 128/255, green: 0/255, blue: 0/255, alpha: 1)
+    
+    lazy var imageView: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "ca"))
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleCaptainSide)))
+        return iv
+    }()
+    
+    lazy var imageView2: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "iron"))
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleIronManSide)))
+        return iv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setTeamsView()
+    }
+}
+
+// MARK: - extension contain actions to select team
+extension PickSideTeamVC {
+    @objc func handleCaptainSide(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleIronManSide(){
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - extensino sets selection view
+extension PickSideTeamVC {
+    func setTeamsView(){
+        view.backgroundColor = .white
+        
+        view.addSubview(imageView)
+        view.addSubview(imageView2)
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        imageView2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView2.topAnchor.constraint(equalTo: view.topAnchor, constant: 400).isActive = true
+        imageView2.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        imageView2.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+}
+
+
+
---
+++ NotificationCenter+Observer/NotificationCenter+Observer/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  NotificationCenter+Observer
+//
+//  Created by Israel Manzo on 5/3/18.
+//  Copyright © 2018 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let civilWarColor = UIColor(red: 64/255, green: 64/255, blue: 64/255, alpha: 1)
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "civil")
+        iv.backgroundColor = .yellow
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    let selectionBtn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .blue
+        btn.setTitle("Select Side", for: .normal)
+        btn.addTarget(self, action: #selector(handleSelectSide), for: .touchUpInside)
+        return btn
+    }()
+    
+    let sideLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pick a Side"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    @objc func handleSelectSide(){
+        let pickSideTeam = PickSideTeamVC()
+        present(pickSideTeam, animated: true, completion: nil)
+        print("Works")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setView()
+    }
+}
+
+// MARK: - extension sets main view
+extension ViewController {
+    
+    func setView(){
+        
+        view.backgroundColor = civilWarColor
+        
+        [selectionBtn, imageView, sideLabel].forEach({view.addSubview($0)})
+        
+        selectionBtn.frame = CGRect(x: 12, y: 600, width: 350, height: 40)
+        selectionBtn.layer.cornerRadius = 5
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 245).isActive = true
+        
+        sideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        sideLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 440).isActive = true
+        sideLabel.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        sideLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
+    }
+}
+
+
+
