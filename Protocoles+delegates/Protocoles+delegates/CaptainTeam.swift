6b682398ed586ef304b9029887ab438c05fd2da6
Israel Manzo <israelmanzo814@gmail.com>
2018-05-02 21:03:08 -0400

    Adding imageview and label

--- Protocoles+delegates/Protocoles+delegates/CaptainTeam.swift
+++ Protocoles+delegates/Protocoles+delegates/CaptainTeam.swift
@@ -8,22 +8,39 @@
 
 import UIKit
 
+protocol SelectYourSideDelegate {
+    func  didSelectSide(img: UIImage, name: String)
+}
+
 class CaptainTeam: UIViewController {
     
-    var selection: UIImage!
+    var selectedSideDelegate: SelectYourSideDelegate!
     
     lazy var imageView: UIImageView = {
-        let iv = UIImageView()
+        let iv = UIImageView(image: UIImage(named: "ca"))
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.isUserInteractionEnabled = true
-        iv.backgroundColor = .blue
-        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleView)))
+        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleCaptainSide)))
         return iv
     }()
     
-    @objc func handleView(){
+    lazy var imageView2: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "iron"))
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleIronManSide)))
+        return iv
+    }()
+    
+    @objc func handleCaptainSide(){
         print("work")
+        selectedSideDelegate.didSelectSide(img: UIImage(named: "team-cap")!, name: "Captain Side")
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleIronManSide(){
+        selectedSideDelegate.didSelectSide(img: UIImage(named: "team-iron")!, name: "Iron Man Side")
+        print("Iron man side")
     }
     
     override func viewDidLoad() {
@@ -32,13 +49,17 @@ class CaptainTeam: UIViewController {
         view.backgroundColor = .white
         
         view.addSubview(imageView)
+        view.addSubview(imageView2)
         
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        print("=========== \(selection)")
-        imageView.image = selection
+        
+        imageView2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView2.topAnchor.constraint(equalTo: view.topAnchor, constant: 400).isActive = true
+        imageView2.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        imageView2.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
     }
 }
--- Protocoles+delegates/Protocoles+delegates/ViewController.swift
+++ Protocoles+delegates/Protocoles+delegates/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .yellow
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
     let selectionBtn: UIButton = {
         let btn = UIButton()
         btn.backgroundColor = .blue
@@ -18,9 +25,17 @@ class ViewController: UIViewController {
         return btn
     }()
     
+    let sideLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pick a Side"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     @objc func handleSelectSide(){
         let captainController = CaptainTeam()
+        captainController.selectedSideDelegate = self
         navigationController?.pushViewController(captainController, animated: true)
         print("Select Side")
     }
@@ -31,9 +46,27 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         view.addSubview(selectionBtn)
+        view.addSubview(imageView)
+        view.addSubview(sideLabel)
         
         selectionBtn.frame = CGRect(x: 85, y: 600, width: 200, height: 40)
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    
+        sideLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        sideLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 500).isActive = true
+        sideLabel.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        sideLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
+}
+
+extension ViewController: SelectYourSideDelegate {
+    func didSelectSide(img: UIImage, name: String) {
+        imageView.image = img
+    }
 }
 
